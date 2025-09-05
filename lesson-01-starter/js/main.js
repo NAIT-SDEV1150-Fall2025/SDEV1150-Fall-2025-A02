f13a62b1a464b37b42c6b31bf35420f4b0c3a229
Daniel Mouris <dgmouris@gmail.com>
2025-09-05 10:46:14 -0600

    class progress pushed

--- lesson-01-starter/js/main.js
+++ lesson-01-starter/js/main.js
@@ -1,1 +1,8 @@
 console.log('Lesson 01 starter loaded');
+
+console.log('Welcome to JavaScript');
+
+// this is a comment
+// we can use a built in function called alert
+// which is is super annoying
+alert("you have won a trip to the bahamas")
