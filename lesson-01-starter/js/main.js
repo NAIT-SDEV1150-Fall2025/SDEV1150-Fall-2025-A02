10056ec5fe6eec56bb74d92610446e831a23deda
Daniel Mouris <dgmouris@gmail.com>
2025-09-05 11:42:06 -0600

    class progress pushed

--- lesson-01-starter/js/main.js
+++ lesson-01-starter/js/main.js
@@ -6,3 +6,16 @@ console.log('Welcome to JavaScript');
 // we can use a built in function called alert
 // which is is super annoying
 // alert("you have won a trip to the bahamas")
+
+// variables in javascript are very similar to variables in python except
+// you put let in front of it.
+let name = "dan"
+
+// we are going to select an element using a css selector
+// we're going to select an element using the query selector
+// we're going to update that text to something new.
+let title = document.querySelector("h1")
+// this selects the element it makes it so that you can use it.
+
+// let's use javascript to update this element
+title.innerHTML = "Updated from JavaScript"
