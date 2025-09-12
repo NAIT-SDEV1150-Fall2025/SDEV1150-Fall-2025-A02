ecf1c93fa0128cf4e0e0038cfb8ef629c65bd27d
Daniel Mouris <dgmouris@gmail.com>
2025-09-12 10:46:55 -0600

    class progress pushed

--- lesson-02-starter/lesson-2/src/main.js
+++ lesson-02-starter/lesson-2/src/main.js
@@ -1,24 +1,6 @@
+// this is something that vite does
+// it bundles up your css along side your javascript
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// let's add a message to the console
+console.log("hello from the console!")
