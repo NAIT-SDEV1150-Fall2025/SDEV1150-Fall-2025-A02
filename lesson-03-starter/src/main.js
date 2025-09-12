13409db1eeee3be2ec3e7d5eabfd4b8a33d1039f
Daniel Mouris <dgmouris@gmail.com>
2025-09-12 11:42:18 -0600

    class progress pushed

--- lesson-03-starter/src/main.js
+++ lesson-03-starter/src/main.js
@@ -9,7 +9,7 @@ console.log(classes)
 
 // 2. Log their types with console.log(typeof …)
 // 3. Try built-in functions: alert(), prompt(), parseInt(), toString()
-alert("My first alert")
+// alert("My first alert")
 // prompt is a to get data from a user
 let favouriteFood = prompt("what is your favourite food?")
 
@@ -20,6 +20,18 @@ console.log('favourite food: ' + favouriteFood)
 let continueDemo = confirm(`hello ${name}, would you like to continue the demo?`)
 
 console.log(`does ${name} choose to continue the demo? ${continueDemo}`)
+
+// let's create a doubler we're going to use parseInt and parseFloat to
+// multiply the code here.
+let numberToDoublePrompt = prompt("Enter a number")
+// we're goingto convert the string that you get from the user to a number
+// we're going to do this with parseFloat
+let numberToDouble = parseFloat(numberToDoublePrompt)
+
+console.log(`
+  double the number ${numberToDouble} is ${numberToDouble*2}
+`)
+
 
 // 4. Manipulate values and observe results in the console
 
