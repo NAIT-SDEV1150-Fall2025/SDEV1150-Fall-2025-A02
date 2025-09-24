f5d94c99c211967bafc5f6bb01a83e24ce3fa378
Daniel Mouris <dgmouris@gmail.com>
2025-09-24 10:33:18 -0600

    class progress pushed

--- lesson-05-starter/src/main.js
+++ lesson-05-starter/src/main.js
@@ -1,6 +1,21 @@
 console.log('Lesson 03 starter loaded');
 
 // 1. Selecting elements
+/*
+I want you to select the following elements
+page title (id selector): <h1 id="page-title">Lesson 05 - Intro to Document Object Model (DOM)</h1>
+tagline (class selector): <p class="tagline">...</p>
+hero img (id selector): <img id="hero-img" src="/manipulation.jpg" alt="Hero image" />
+caption (id selector): <p id="hero-caption">...</p>
+
+to do this we're going to use the querySelector
+*/
+const titleEl = document.querySelector('#page-title');
+
+// print out all values to the console
+console.log('values', {
+  titleEl,
+});
 
 // 2. textContent vs innerHTML
 
