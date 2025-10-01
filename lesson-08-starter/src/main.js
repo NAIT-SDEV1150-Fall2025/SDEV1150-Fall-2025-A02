e7769e3d9ec29c70082fc285b27ac1dffd31e8c5
Daniel Mouris <dgmouris@gmail.com>
2025-10-01 11:25:56 -0600

    class progress pushed

--- lesson-08-starter/src/main.js
+++ lesson-08-starter/src/main.js
@@ -1,18 +1,59 @@
 console.log('Lesson 08 starter loaded');
 
 // 1. load event (document ready) - NOTE this is unnecessary if using `defer` in the script tag or using module type
-// the window contains information about the browser
+// the window contains information about the browser, everything in js.
 // the page and where you're at.
 console.log(window);
+// add event listener takes two arguments
+// 'DOMContentLoaded' is the event type (first arg)
+// () => { /* ... */ } is the function (second arg)
+// all event listeners look like this.
+// note this is not needed if you use defer or using type="module"
+window.addEventListener('DOMContentLoaded', () => {
+  // 2. Selecting elements
+  /* select with the following ids
+  btn-toggle
+  btn-message
+  message
+  hover-card
+  key-output
+  list
+  selection
+  */
+  const btnToggle = document.querySelector('#btn-toggle');
+  const btnMessage = document.querySelector('#btn-message');
+  const message = document.querySelector('#message');
+  const hoverCard = document.querySelector('#hover-card');
+  const keyOutput = document.querySelector('#key-output');
+  const list = document.querySelector('#list');
+  const selection = document.querySelector('#selection');
 
-// 2. Selecting elements
+  console.log({
+    btnToggle,
+    btnMessage,
+    message,
+    hoverCard,
+    keyOutput,
+    list,
+    selection,
+  });
 
-// 3. click: toggle a highlight class on the body
+  // 3. click: toggle a highlight class on the body
+  // 'click' means when you actually use the mouse to click
+  btnToggle.addEventListener('click', () => {
+    // we're going to add the toggle on the body
+    // on classList there's a function called toggle
+    document.body.classList.toggle('highlight');
+    // we're check to see if that element contains the class (with classList)
+  });
 
-// 4. click: change message textContent (no HTML parsing)
+  // 4. click: change message textContent (no HTML parsing)
 
-// 5. mouseover / mouseout: display hover status on the card
+  // 5. mouseover / mouseout: display hover status on the card
 
-// 6. keydown: show last key pressed (global listener)
+  // 6. keydown: show last key pressed (global listener)
 
-// 7. Event delegation: one listener on the <ul> for all <li> elements
+  // 7. Event delegation: one listener on the <ul> for all <li> elements
+
+});
+
