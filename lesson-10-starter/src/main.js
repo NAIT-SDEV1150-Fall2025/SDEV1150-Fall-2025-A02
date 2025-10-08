42af45a4afa5bdd573714c716c56c42d3ebc6a5e
Daniel Mouris <dgmouris@gmail.com>
2025-10-08 11:40:11 -0600

    class progress pushed

--- lesson-10-starter/src/main.js
+++ lesson-10-starter/src/main.js
@@ -1,6 +1,8 @@
 console.log('Lesson 10 starter loaded');
 
 // 1. Select required elements
+const form = document.querySelector('#contact-form');
+const result = document.querySelector('#result');
 
 // 2. Function to gather and structure form data
 
@@ -9,5 +11,13 @@ console.log('Lesson 10 starter loaded');
 // 3. Handle form submission
 // Use 'submit' event on the form, not 'click' on the button
 // Prevent default behavior (navigation/reload) using event.preventDefault()
+form.addEventListener('submit', (event) => {
+  // we learned last class about stopPropagation
+  // in this class we don't want the page to execute the
+  // default action of the form when I click submit
+  // we can do this with event.preventDefault()
+  event.preventDefault();
+  console.log('not refreshed');
+});
 
 // 4. Handle form reset - reset the result area text when the form is reset
