23be5bb593e92971f2ee45fecb0a74486979105c
Daniel Mouris <dgmouris@gmail.com>
2025-10-17 11:45:49 -0600

    class progress pushed

--- lesson-13-starter/main.js
+++ lesson-13-starter/main.js
@@ -1,13 +1,13 @@
 // we're create the main.js file.
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 // let's get todays date
-let now = dayjs()
+let now = dayjs();
 
-console.log("A node example")
-console.log(`time: ${now}`)
-console.log(`the date is ${now.format('YYYY-MM-DD')}`)
-console.log(`the the time is ${now.format('HH:mm:ss')}`)
+console.log('A node example');
+console.log(`time: ${now}`);
+console.log(`the date is ${now.format('YYYY-MM-DD')}`);
+console.log(`the the time is ${now.format('HH:mm:ss')}`);
 // refernce docs: https://day.js.org/docs/en/display/format#docsNav
 
 // we're going to run this project
