6416578ce316c0ea75c0125e3b29e449ad4c8c0c
Daniel Mouris <dgmouris@gmail.com>
2025-10-29 10:38:14 -0600

    class progress pushed

--- lesson-16-starter/src/utils.js
+++ lesson-16-starter/src/utils.js
@@ -1,4 +1,25 @@
 // Fetch utility function
+// fetch is what's called async
+// async/await these are special commands that allow you to fulfill promises
+// we are "fetching" over the network to the backend which can
+// fail or succeed, but this also takes time.
+export async function getData(endpoint) {
+  // to make the request we need to explicitly "await"
+  const response = await fetch(endpoint, { method: 'GET' });
+  // this is going to give you all of the information about the response itself.
+  // we're going to throw an error if the response is not ok.
+  console.log('Response', response);
+  if (!response.ok) {
+    // you can raise errors by "throwing" them
+    throw new Error('Network response failed');
+    // this above can only be caught by a try ... catch
+  }
+  // the response we get a json string, response has a method called .json()
+  // which is going to parse the json into a useable javascript object.
+  const data = response.json();
+  console.log('data', data);
+  return data;
+}
 
 // POST utility function
 
