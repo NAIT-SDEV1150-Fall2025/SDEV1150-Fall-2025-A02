66b5f46892744ef961e62ecbdb408bb996b52cc3
Daniel Mouris <dgmouris@gmail.com>
2025-11-12 10:32:07 -0700

    class progress pushed

--- lesson-19-starter/src/main.js
+++ lesson-19-starter/src/main.js
@@ -1,7 +1,7 @@
 // Import the user-card component to register the custom element
 import './user-card.js';
 
-console.log(UserCard);
+
 // Create an additional user card using HTML and append it to the main element
 
 // Create another user card using JavaScript DOM API only and append it to the main element
