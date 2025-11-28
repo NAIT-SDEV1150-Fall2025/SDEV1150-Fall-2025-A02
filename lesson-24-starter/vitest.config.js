023b474628fef8c0260e46bc950762134c8286d8
Daniel Mouris <dgmouris@gmail.com>
2025-11-28 10:51:45 -0700

    class progress pushed

---
+++ lesson-24-starter/vitest.config.js
@@ -0,0 +1,13 @@
+// a file that will configure the vitest package.
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  // we're going to specify a test environment
+  test: {
+    environment: 'jsdom',
+  },
+});
+// we need to make node.js understand how to parse
+// js and html because we're going to be testing
+// in the terminal.
