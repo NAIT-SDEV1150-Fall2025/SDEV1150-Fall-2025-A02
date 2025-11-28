f6acd706e1497ad433b9d92af9a422a23db326b9
Daniel Mouris <dgmouris@gmail.com>
2025-11-28 11:17:48 -0700

    class progress pushed

---
+++ lesson-24-starter/__tests__/user-card.test.js
@@ -0,0 +1,11 @@
+import { expect, describe, test } from 'vitest';
+// we're going to import our card.
+import '../src/user-card.js';
+
+// let's create a section
+describe('UserCard', () => {
+  // we're going to put a few tests.
+  test('renders with the default properties', () => {
+
+  });
+})
